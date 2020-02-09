b2c9dedadf6cbbfbf39b0f3b98074648f0e279ca
Aeroneeee <pamintuan.aerone@gmail.com>
2020-02-09 14:16:50 +0800

    Saving

--- FormToFile/Form1.Designer.vb
+++ FormToFile/Form1.Designer.vb
@@ -22,96 +22,105 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.name = New System.Windows.Forms.TextBox()
+        Me.components = New System.ComponentModel.Container()
+        Me.nametxtbox = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.closeBtn = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.age = New System.Windows.Forms.TextBox()
+        Me.agetxtbox = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.address = New System.Windows.Forms.TextBox()
-        Me.Text = New System.Windows.Forms.PictureBox()
+        Me.addresstxtbox = New System.Windows.Forms.TextBox()
+        Me.Txt = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Xml = New System.Windows.Forms.PictureBox()
         Me.Json = New System.Windows.Forms.PictureBox()
         Me.Label5 = New System.Windows.Forms.Label()
-        CType(Me.Text, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ErrorProvider1 = New System.Windows.Forms.ErrorProvider(Me.components)
+        CType(Me.Txt, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Xml, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Json, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'name
+        'nametxtbox
         '
-        Me.name.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.name.Location = New System.Drawing.Point(26, 96)
-        Me.name.Name = "name"
-        Me.name.Size = New System.Drawing.Size(273, 30)
-        Me.name.TabIndex = 0
+        Me.nametxtbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nametxtbox.Location = New System.Drawing.Point(26, 96)
+        Me.nametxtbox.Name = "nametxtbox"
+        Me.nametxtbox.Size = New System.Drawing.Size(273, 30)
+        Me.nametxtbox.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(23, 80)
+        Me.Label1.Font = New System.Drawing.Font("Provicali", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 76)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.Size = New System.Drawing.Size(52, 18)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Name"
+        Me.Label1.Text = "NAME"
         '
-        'Label2
+        'closeBtn
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Label2.Location = New System.Drawing.Point(266, 412)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(33, 13)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Close"
+        Me.closeBtn.AutoSize = True
+        Me.closeBtn.BackColor = System.Drawing.Color.Transparent
+        Me.closeBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.closeBtn.Font = New System.Drawing.Font("Comfortaa", 8.249999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.closeBtn.Location = New System.Drawing.Point(266, 412)
+        Me.closeBtn.Name = "closeBtn"
+        Me.closeBtn.Size = New System.Drawing.Size(39, 17)
+        Me.closeBtn.TabIndex = 2
+        Me.closeBtn.Text = "Close"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(23, 153)
+        Me.Label3.Font = New System.Drawing.Font("Provicali", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(23, 149)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.Size = New System.Drawing.Size(39, 18)
         Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Age"
+        Me.Label3.Text = "AGE"
         '
-        'age
+        'agetxtbox
         '
-        Me.age.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.age.Location = New System.Drawing.Point(26, 169)
-        Me.age.Name = "age"
-        Me.age.Size = New System.Drawing.Size(273, 30)
-        Me.age.TabIndex = 3
+        Me.agetxtbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.agetxtbox.Location = New System.Drawing.Point(26, 169)
+        Me.agetxtbox.MaxLength = 3
+        Me.agetxtbox.Name = "agetxtbox"
+        Me.agetxtbox.Size = New System.Drawing.Size(273, 30)
+        Me.agetxtbox.TabIndex = 3
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(23, 227)
+        Me.Label4.Font = New System.Drawing.Font("Provicali", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(23, 224)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.Size = New System.Drawing.Size(81, 18)
         Me.Label4.TabIndex = 6
-        Me.Label4.Text = "Address"
+        Me.Label4.Text = "ADDRESS"
         '
-        'address
+        'addresstxtbox
         '
-        Me.address.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.address.Location = New System.Drawing.Point(26, 243)
-        Me.address.Name = "address"
-        Me.address.Size = New System.Drawing.Size(273, 30)
-        Me.address.TabIndex = 5
+        Me.addresstxtbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.addresstxtbox.Location = New System.Drawing.Point(26, 243)
+        Me.addresstxtbox.Name = "addresstxtbox"
+        Me.addresstxtbox.Size = New System.Drawing.Size(273, 30)
+        Me.addresstxtbox.TabIndex = 5
         '
-        'Text
+        'Txt
         '
-        Me.Text.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Text.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Text.Image = Global.FormToFile.My.Resources.Resources.txt
-        Me.Text.Location = New System.Drawing.Point(26, 310)
-        Me.Text.Name = "Text"
-        Me.Text.Size = New System.Drawing.Size(70, 85)
-        Me.Text.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.Text.TabIndex = 8
-        Me.Text.TabStop = False
+        Me.Txt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Txt.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Txt.Image = Global.FormToFile.My.Resources.Resources.txt
+        Me.Txt.Location = New System.Drawing.Point(26, 300)
+        Me.Txt.Name = "Txt"
+        Me.Txt.Size = New System.Drawing.Size(70, 85)
+        Me.Txt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Txt.TabIndex = 8
+        Me.Txt.TabStop = False
         '
         'PictureBox1
         '
@@ -127,7 +136,7 @@ Partial Class Form1
         '
         Me.Xml.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Xml.Image = Global.FormToFile.My.Resources.Resources.xml
-        Me.Xml.Location = New System.Drawing.Point(126, 310)
+        Me.Xml.Location = New System.Drawing.Point(126, 300)
         Me.Xml.Name = "Xml"
         Me.Xml.Size = New System.Drawing.Size(70, 85)
         Me.Xml.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -138,7 +147,7 @@ Partial Class Form1
         '
         Me.Json.Cursor = System.Windows.Forms.Cursors.Hand
         Me.Json.Image = Global.FormToFile.My.Resources.Resources.json
-        Me.Json.Location = New System.Drawing.Point(228, 310)
+        Me.Json.Location = New System.Drawing.Point(228, 300)
         Me.Json.Name = "Json"
         Me.Json.Size = New System.Drawing.Size(70, 85)
         Me.Json.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -148,12 +157,17 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Tahoma", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Font = New System.Drawing.Font("Keep Calm Med", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ErrorProvider1.SetIconAlignment(Me.Label5, System.Windows.Forms.ErrorIconAlignment.TopLeft)
         Me.Label5.Location = New System.Drawing.Point(20, 25)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(164, 33)
+        Me.Label5.Size = New System.Drawing.Size(248, 37)
         Me.Label5.TabIndex = 11
-        Me.Label5.Text = "Create A File"
+        Me.Label5.Text = "CREATE A FILE"
+        '
+        'ErrorProvider1
+        '
+        Me.ErrorProvider1.ContainerControl = Me
         '
         'Form1
         '
@@ -164,42 +178,43 @@ Partial Class Form1
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Json)
         Me.Controls.Add(Me.Xml)
-        Me.Controls.Add(Me.Text)
+        Me.Controls.Add(Me.Txt)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.address)
+        Me.Controls.Add(Me.addresstxtbox)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.age)
-        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.agetxtbox)
+        Me.Controls.Add(Me.closeBtn)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.name)
+        Me.Controls.Add(Me.nametxtbox)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.name = "Form1"
+        Me.Name = "Form1"
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
         Me.TransparencyKey = System.Drawing.SystemColors.ButtonFace
-        CType(Me.Text, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Txt, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Xml, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Json, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents name As TextBox
+    Friend WithEvents nametxtbox As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents closeBtn As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents age As TextBox
+    Friend WithEvents agetxtbox As TextBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents address As TextBox
+    Friend WithEvents addresstxtbox As TextBox
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Text As PictureBox
+    Friend WithEvents Txt As PictureBox
     Friend WithEvents Xml As PictureBox
     Friend WithEvents Json As PictureBox
     Friend WithEvents Label5 As Label
+    Friend WithEvents ErrorProvider1 As ErrorProvider
 End Class
--- FormToFile/Form1.vb
+++ FormToFile/Form1.vb
@@ -1,10 +1,114 @@
 ﻿Public Class Form1
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-        Dim frm As New Form1
-        frm.Show()
 
-        Me.Close()
+    Dim countTxt, countXml, countJson As Byte
+    Dim ToFile As String
+
+    Private Sub closeBtn_Click(sender As Object, e As EventArgs) Handles closeBtn.Click
+        Application.Exit()
+    End Sub
+    Public Sub NumberOnly(ByVal e As System.Windows.Forms.KeyPressEventArgs)
+
+        If (Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57 Or Asc(e.KeyChar) = 8) Then
+            ErrorProvider1.Clear()
+
+        Else
+            e.Handled = True
+            ErrorProvider1.SetError(agetxtbox, "Please Enter Number Only")
+
+        End If
+
     End Sub
 
-    
+    Private Sub agetxtbox_KeyPress(sender As Object, e As EventArgs) Handles agetxtbox.KeyPress
+        NumberOnly(e)
+    End Sub
+
+    Private Sub Txt_Click(sender As Object, e As EventArgs) Handles Txt.Click
+
+        If SaveFileAs(countTxt, ".txt") Then
+            countTxt += 1
+        End If
+
+    End Sub
+
+
+    Private Sub Xml_Click(sender As Object, e As EventArgs) Handles Xml.Click
+
+        If SaveFileAs(countXml, ".xml") Then
+            countXml += 1
+        End If
+
+    End Sub
+
+
+    Private Sub Json_Click(sender As Object, e As EventArgs) Handles Json.Click
+
+        If SaveFileAs(countJson, ".json") Then
+            countJson += 1
+        End If
+
+    End Sub
+
+    Private Function SaveFileAs(count As Byte, fileType As String)
+        If nametxtbox.Text <> "" And agetxtbox.Text <> "" And addresstxtbox.Text <> "" Then
+
+            ToFile = String.Concat(nametxtbox.Text,
+                                   Environment.NewLine,
+                                   agetxtbox.Text,
+                                   Environment.NewLine,
+                                   addresstxtbox.Text)
+
+            My.Computer.FileSystem.WriteAllText("C:\Users\Aerone\Documents\Visual Studio 2019\FormToFile\SavedFile" & count & fileType, "" & ToFile, True)
+
+            MsgBox("Successfully saved the file.", MsgBoxStyle.Information, "Successfully Saved")
+
+            Return True
+
+        Else
+
+            MsgBox("Please write all information.", MsgBoxStyle.Critical, "Input Data")
+            Return False
+
+        End If
+
+    End Function
+
+    Private Sub closeBtn_MouseHover(sender As Object, e As EventArgs) Handles closeBtn.MouseHover
+        closeBtn.BackColor = Color.Pink
+    End Sub
+
+    Private Sub closeBtn_MouseLeave(sender As Object, e As EventArgs) Handles closeBtn.MouseLeave
+        closeBtn.BackColor = Color.Transparent
+    End Sub
+
+    Private Sub Txt_MouseHover(sender As Object, e As EventArgs) Handles Txt.MouseHover
+        Txt.Size = New Size(75, 90)
+        Txt.Location = New Point(24, 298)
+    End Sub
+
+    Private Sub Txt_MouseLeave(sender As Object, e As EventArgs) Handles Txt.MouseLeave
+        Txt.Size = New Size(70, 85)
+        Txt.Location = New Point(26, 300)
+    End Sub
+
+    Private Sub Xml_MouseHover(sender As Object, e As EventArgs) Handles Xml.MouseHover
+        Xml.Size = New Size(75, 90)
+        Xml.Location = New Point(124, 298)
+    End Sub
+
+    Private Sub Xml_MouseLeave(sender As Object, e As EventArgs) Handles Xml.MouseLeave
+        Xml.Size = New Size(70, 85)
+        Xml.Location = New Point(126, 300)
+    End Sub
+
+    Private Sub Json_MouseHover(sender As Object, e As EventArgs) Handles Json.MouseHover
+        Json.Size = New Size(75, 90)
+        Json.Location = New Point(226, 298)
+    End Sub
+
+    Private Sub Json_MouseLeave(sender As Object, e As EventArgs) Handles Json.MouseLeave
+        Json.Size = New Size(70, 85)
+        Json.Location = New Point(228, 300)
+    End Sub
+
 End Class
